54c632ec0a84f216bb799719d213a14c12240f55
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2023-10-24 16:07:13 +0400

    Added script functionality and style

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
 const apiKey = "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";
+const maxImages = 9;
+const generateInfo = document.getElementById("generate");
+const imageGrid = document.getElementById("image-grid");
+const loading = document.getElementById("loading");
+
+async function generateImages(input) {
+    generateInfo.disabled = true;
+    imageGrid.innerHTML = "";
+    loading.style.display = "block";
+
+    const imageUrls = [];
+
+    for (let i = 0; i < maxImages; i++) {
+        const randomNumber = Math.floor(Math.random() * (10000 - 1 + 1)) + 1;
+        const prompt = `${input} ${randomNumber}`;
+
+        const response = await fetch(
+            "https://api-inference.huggingface.co/models/prompthero/openjourney",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${apiKey}`,
+                },
+                body: JSON.stringify({ inputs: prompt }),
+            }
+        );
+
+        if (!response.ok) {
+            alert("Failed to generate image!");
+        }
+
+        const blob = await response.blob();
+        const imgUrl = URL.createObjectURL(blob);
+        imageUrls.push(imgUrl);
+
+        const img = document.createElement("img");
+        img.src = imgUrl;
+        img.alt = `art-${i + 1}`;
+        img.onclick = () => downloadImage(imgUrl, i);
+        imageGrid.appendChild(img);
+    }
+
+    loading.style.display = "none";
+    generateInfo.disabled = false;
+}
+
+generateInfo.addEventListener('click', () => {
+    const input = document.getElementById("user-prompt").value;
+    generateImages(input);
+});
+
+function downloadImage(imgUrl, imageNumber) {
+    const link = document.createElement("a");
+    link.href = imgUrl;
+    link.download = `image-${imageNumber + 1}.jpg`;
+    link.click();
+}
